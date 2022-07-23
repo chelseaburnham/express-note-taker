cfbf560f6a851a38128a8cdc125f054bb0adfbfe
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-22 19:03:56 -0700

    added middleware

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
