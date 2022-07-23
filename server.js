52977ca0428a9cca9488adc595233e2366fb96a2
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-22 19:04:54 -0700

    added comments for middleware

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+//variable declaration
 const express = require("express");
 const path = require("path");
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
