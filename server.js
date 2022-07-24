87b6bf88d72dbe58d7cfc3353d2e16875edc4edf
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-23 18:37:46 -0700

    installed uuid

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //variable declaration
+const { notStrictEqual } = require("assert");
 const express = require("express");
 const path = require("path");
 const app = express();
+const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 
 //middleware
